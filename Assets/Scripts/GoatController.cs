196eb4a9c70cc01c13e2a4e8d25fb533182040f8
MaliaWilken4 <mawilken4@gmail.com>
2025-09-17 00:41:49 -0700

    Background + GoatController.cs + Basic TileMap

--- Assets/Scripts/GoatController.cs
+++ Assets/Scripts/GoatController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class goatMovement : MonoBehaviour
 {
+    public float speed;
+
+    public Rigidbody2D body;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class goatMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
         
+        if (Mathf.Abs(xInput) > 0)
+        {
+            body.velocity = new Vector2(xInput*speed, body.velocity.y);
+        }
+
+        if (Mathf.Abs(yInput) > 0)
+        {
+            body.velocity = new Vector2(body.velocity.x, yInput*speed);
+        }
     }
 }
