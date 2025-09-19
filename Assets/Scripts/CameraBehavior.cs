bcec5f84d0ab60b30acc5ec0a7be4f7179e59833
MaliaWilken4 <mawilken4@gmail.com>
2025-09-19 01:41:11 -0700

    Parallax Background

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -1,59 +1,59 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using Unity.VisualScripting;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
-//public class Parallax : MonoBehaviour
-//{
-//    public Camera cam;
-//    public Transform subject;
+public class Parallax : MonoBehaviour
+{
+    public Camera cam;
+    public Transform subject;
 
-//    public Vector2 deadZone = new Vector2(2f, 1.5f);
-//    public float minY = -2f;
+    //public Vector2 deadZone = new Vector2(2f, 1.5f);
+    //public float minY = -2f;
 
-//    private Vector2 startPosition;
+    private Vector2 startPosition;
 
-//    private float startZ;
+    private float startZ;
 
-//    private Vector3 cameraOffset;
+    //private Vector3 cameraOffset;
 
-//    //Vector2 travel => (Vector2)cam.transform.position - startPosition;
+    //Vector2 travel => (Vector2)cam.transform.position - startPosition;
 
-//    public void Start()
-//    {
-//        startPosition = transform.position;
-//        startZ = transform.position.z;
-//        cameraOffset = cam.transform.position - subject.position;
-//    }
+    public void Start()
+    {
+        startPosition = transform.position;
+        startZ = transform.position.z;
+        //cameraOffset = cam.transform.position - subject.position;
+    }
 
-//    //public void Update()
-//    //{
-//    //    Vector2 newPos = startPosition + new Vector2(travel.x, 0);
-//    //    transform.position = new Vector3(newPos.x, startPosition.y, 0);
-//    //}
+    //public void Update()
+    //{
+    //    Vector2 newPos = startPosition + new Vector2(travel.x, 0);
+    //    transform.position = new Vector3(newPos.x, startPosition.y, 0);
+    //}
 
-//    public void LateUpdate()
-//    {
-//        Vector3 desired = cam.transform.position;
+    public void LateUpdate()
+    {
+        Vector3 desired = cam.transform.position;
 
-//        // Horizontal Camera Movement
-//        if (Mathf.Abs(subject.position.x - cam.transform.position.x) > deadZone.x)
-//        {
-//            desired.x = subject.position.x + cameraOffset.x;
-//        }
+        //// Horizontal Camera Movement
+        //if (Mathf.Abs(subject.position.x - cam.transform.position.x) > deadZone.x)
+        //{
+        //    desired.x = subject.position.x + cameraOffset.x;
+        //}
 
-//        // Vertical Camera Movement
-//        if (Mathf.Abs(subject.position.y - cam.transform.position.y) > deadZone.y)
-//        {
-//            desired.y = Mathf.Max(subject.position.y + cameraOffset.y, minY);
-//        }
+        //// Vertical Camera Movement
+        //if (Mathf.Abs(subject.position.y - cam.transform.position.y) > deadZone.y)
+        //{
+        //    desired.y = Mathf.Max(subject.position.y + cameraOffset.y, minY);
+        //}
 
-//        cam.transform.position = desired;
+        //cam.transform.position = desired;
 
-//        // Parallax Effect
-//        Vector2 travel = (Vector2)cam.transform.position - startPosition;
-//        Vector2 newPos = startPosition + new Vector2(travel.x, 0);          // Horizontal Movement Only
-//        transform.position = new Vector3(newPos.x, startPosition.y, startZ);
-//    }
+        // Parallax Effect
+        Vector2 travel = (Vector2)cam.transform.position - startPosition;
+        Vector2 newPos = startPosition + new Vector2(travel.x, 0);          // Horizontal Movement Only
+        transform.position = new Vector3(newPos.x, startPosition.y, startZ);
+    }
 
-//}
+}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,11 +8,12 @@ public class CameraFollow : MonoBehaviour
     public float yOffset = 1f;
     public Transform target;
     public float yMin = -1f;
+    public float yMax = 1.7f;
 
     // Update is called once per frame
     void Update()
     {
-        if (target.position.y >= yMin)
+        if (target.position.y >= yMin && target.position.y <= yMax)
         {
             Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
             transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
